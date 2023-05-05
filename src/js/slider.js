2c61b3ce7f3d613409f4230c4b0c0cd27f0a9495
Fatiuk <7777bmx7777@gmail.com>
2023-05-05 13:16:07 +0300

    Update 0.7
    Fix logo
    Fix slider JS
    Fix favicon

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,16 +1,27 @@
-    var swiper = new Swiper(".mySwiper", {
-      spaceBetween: 30,
+var swiper = new Swiper('.mySwiper', {
+  spaceBetween: 30,
       centeredSlides: true,
       autoplay: {
         delay: 2500,
         disableOnInteraction: false,
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
+  },
+  navigation: {
+    nextEl: '.hero-btn-slider-next',
+    prevEl: '.hero-btn-slider-prev',
+  },
+  pagination: {
+    el: '.hero-swiper-pagination',
+    type: 'fraction',
+
+    renderFraction: function (currentClass, totalClass) {
+      const line = '&nbsp;&#9135;&#9135;&#9135;&#9135;&#9135;&nbsp;';
+      return `0<span class="${currentClass}"></span> ${line} 0<span class="${totalClass}"></span>`;
+    },
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+  loop: true,
+});
