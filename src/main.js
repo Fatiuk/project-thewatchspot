6ebe7e0c020d902b5b0a290a08c2af4dd81930b2
Fatiuk <7777bmx7777@gmail.com>
2023-05-08 21:53:52 +0300

    Update 0.10
    AdditIonal

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@
 import "./js/slider.js"
 //* Mobile menu in header section
 import "./js/mobile-menu.js"
+//* Scroll button
+import "./js/scroll.js"
 //* Modal in category and popup section
 import "./js/modal.js"
