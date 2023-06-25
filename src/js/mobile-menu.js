07bd10b4eadfdaee332b0f966cd01582b91db243
Fatiuk <7777bmx7777@gmail.com>
2023-06-25 16:44:41 +0300

    Release (fix) 1.1

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,18 +1,14 @@
 (() => {
-  const mobileMenu = document.querySelector("[data-menu]");
-  const openMenuBtn = document.querySelector("[data-menu-open]");
-  const closeMenuBtn = document.querySelector("[data-menu-close]");
+  const mobileMenu = document.querySelector('[data-menu]');
+  const openMenuBtn = document.querySelector('[data-menu-open]');
+  const closeMenuBtn = document.querySelector('[data-menu-close]');
 
   const toggleMenu = () => {
     const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+      openMenuBtn.getAttribute('aria-expanded') === true || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
-
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
+    document.body.classList.toggle('no-scroll');
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
