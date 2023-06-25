33b7cdc8405dbe3edb6fbfca9fd9f24d1a632b34
Fatiuk <7777bmx7777@gmail.com>
2023-06-25 16:56:52 +0300

    Additional fix 1.1

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -19,6 +19,5 @@
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
